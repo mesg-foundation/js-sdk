3e8da34f92f2ec3436cbe5a95c2a5881525708b8
Anthony ESTEBE <anthony.estebe@gmail.com>
2019-02-21 00:12:46 +0700

    move mesg logic to root command

--- src/root-command.ts
+++ src/root-command.ts
@@ -1,7 +1,50 @@
 import {Command, flags} from '@oclif/command'
+import {application} from 'mesg-js'
+
+type UNARY_METHODS = 'DeleteService'
+  | 'GetService'
+  | 'ListServices'
+  | 'StartService'
+  | 'StopService'
+  | 'Info'
+
+interface ExecutionResult {
+  output: string
+  data: any
+}
 
 export default abstract class extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
+  }
+
+  protected mesg = application({
+    endpoint: 'localhost:50052'
+  })
+
+  async execute(serviceID: string, taskKey: string, data: object = {}): Promise<ExecutionResult> {
+    this.debug(`Execute task ${taskKey} from ${serviceID} with ${JSON.stringify(data)}`)
+    const result = await this.mesg.executeTaskAndWaitResult({
+      serviceID,
+      taskKey,
+      inputData: JSON.stringify(data),
+      executionTags: ['cli']
+    })
+    if (result.error) {
+      this.error(result.error)
+      throw result.error
+    }
+    this.debug(`Receiving result of ${result.executionID}, ${result.outputKey} ${result.taskKey} => ${result.outputData}`)
+    return {
+      data: JSON.parse(result.outputData),
+      output: result.outputKey,
+    }
+  }
+
+  async unaryCall(method: UNARY_METHODS, data: object = {}): Promise<any> {
+    this.debug(`Call MESG Core API ${method} with ${JSON.stringify(data)}`)
+    return new Promise((resolve, reject) => this.mesg.api[method](data, (error: Error, res: any) => error
+      ? reject(error)
+      : resolve(res)))
   }
 }
--- src/service-command.ts
+++ src/service-command.ts
@@ -1,5 +1,3 @@
-import {application} from 'mesg-js'
-
 import Command from './root-command'
 
 export interface Service {
@@ -11,28 +9,12 @@ export interface Service {
 
 export type SERVICE_PARAMETER_TYPE = 'String' | 'Number' | 'Boolean' | 'Object' | 'Any'
 
-type UNARY_METHODS = 'DeleteService'
-  | 'GetService'
-  | 'ListServices'
-  | 'StartService'
-  | 'StopService'
-
 export default abstract class extends Command {
   static flags = {
     ...Command.flags
   }
 
-  protected mesg = application({
-    endpoint: 'localhost:50052'
-  })
-
   status(s: number) {
     return ['unknown', 'stopped', 'starting', 'partial', 'running'][s]
-  }
-
-  async unaryCall(method: UNARY_METHODS, data: object): Promise<any> {
-    return new Promise((resolve, reject) => this.mesg.api[method](data, (error: Error, res: any) => error
-      ? reject(error)
-      : resolve(res)))
   }
 }
