b42da373719c65ff7809ac16ec96a5b84556cd5d
Anthony ESTEBE <anthony@mesg.com>
2019-07-02 16:07:23 +0700

    daemon commands no-follow

--- src/commands/daemon/logs.ts
+++ src/commands/daemon/logs.ts
@@ -11,6 +11,11 @@ export default class Logs extends Command {
     tail: flags.integer({
       description: 'Output specified number of lines at the end of logs',
       default: -1
+    }),
+    follow: flags.boolean({
+      description: 'Follow logs',
+      allowNo: true,
+      default: true
     })
   }
 
@@ -24,7 +29,7 @@ export default class Logs extends Command {
     const logs: any = await service.logs({
       stderr: true,
       stdout: true,
-      follow: true,
+      follow: flags.follow,
       tail: flags.tail && flags.tail >= 0 ? flags.tail : 'all'
     })
     logs.on('data', (buffer: Buffer) => parseLog(buffer).forEach(x => this.log(x)))
