3b31a9d4dc2a78d3a6e32c4cf71640c7bab45438
Anthony ESTEBE <anthony@mesg.com>
2019-07-26 23:56:07 +0700

    add start flag to command create

--- src/commands/service/create.ts
+++ src/commands/service/create.ts
@@ -1,12 +1,19 @@
+import {flags} from '@oclif/command'
 import {ServiceCreateOutputs} from 'mesg-js/lib/api'
 
 import Command from '../../root-command'
+
+import ServiceStart from './start'
 
 export default class ServiceCreate extends Command {
   static description = 'Create a service'
 
   static flags = {
     ...Command.flags,
+    start: flags.boolean({
+      description: 'Automatically start the service once created',
+      default: false,
+    })
   }
 
   static args = [{
@@ -16,11 +23,16 @@ export default class ServiceCreate extends Command {
   }]
 
   async run(): ServiceCreateOutputs {
-    const {args} = this.parse(ServiceCreate)
+    const {args, flags} = this.parse(ServiceCreate)
     this.spinner.start('Create service')
     const resp = await this.api.service.create(JSON.parse(args.DEFINITION))
     if (!resp.hash) { throw new Error('invalid response') }
     this.spinner.stop(resp.hash)
+    if (flags.start) {
+      this.spinner.start('Create service')
+      const start = await ServiceStart.run([resp.hash])
+      this.spinner.stop(start.hash)
+    }
     return resp
   }
 }
