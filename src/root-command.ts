725c9efeca31fc6ff396c25ae56969864df30be6
Anthony ESTEBE <anthony@mesg.com>
2019-07-30 17:58:33 +0700

    update root command execute

--- src/root-command.ts
+++ src/root-command.ts
@@ -2,7 +2,7 @@ import {Command, flags} from '@oclif/command'
 import {IConfig} from '@oclif/config'
 import {cli} from 'cli-ux'
 import {application} from 'mesg-js'
-import createApi, {API, ExecutionCreateInputs, InfoOutputs} from 'mesg-js/lib/api'
+import createApi, {API, InfoOutputs} from 'mesg-js/lib/api'
 import {Application} from 'mesg-js/lib/application'
 import {format, inspect} from 'util'
 
@@ -68,9 +68,16 @@ export default abstract class extends Command {
     }
   }
 
-  async execute(request: ExecutionCreateInputs): Promise<any> {
-    const exec = await this._app.executeTaskAndWaitResult(request)
-    return JSON.parse(exec.outputs || '')
+  async execute(request: {instanceHash: string, taskKey: string, inputs: {[key: string]: any}, tags?: string[]}): Promise<{[key: string]: any}> {
+    const exec = await this._app.executeTaskAndWaitResult({
+      instanceHash: request.instanceHash,
+      tags: request.tags || [],
+      taskKey: request.taskKey,
+      inputs: this._app.encodeData(request.inputs)
+    })
+    if (exec.error) throw new Error(exec.error)
+    if (!exec.outputs) throw new Error('missing outputs')
+    return this._app.decodeData(exec.outputs)
   }
 
   async info(): InfoOutputs {
