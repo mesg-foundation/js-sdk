04af7c7b55258adddf3525a3a97a62de5626e6ae
Anthony ESTEBE <anthony@mesg.com>
2019-11-08 12:11:49 +0700

    add lerna and api package

---
+++ packages/api/src/util/grpc_test.ts
@@ -0,0 +1,27 @@
+import { Test } from 'tape'
+import test from 'tape'
+import { checkStreamReady } from './grpc'
+
+test('checkStreamReady() without error', function (t: Test) {
+  t.plan(2);
+  t.false(checkStreamReady({
+    get(key) {
+      t.equal(key, 'status')
+      return ['x', 'ready']
+    }
+  }))
+});
+
+test('checkStreamReady() with empty statuses', function (t: Test) {
+  t.plan(1);
+  t.equal(checkStreamReady({
+    get(key) { return [] }
+  }).message, 'stream header does not contain any status')
+});
+
+test('checkStreamReady() without having ready status', function (t: Test) {
+  t.plan(1);
+  t.equal(checkStreamReady({
+      get(key) { return ['x'] }
+  }).message, `stream header status is different than ready. Got x`)
+});
