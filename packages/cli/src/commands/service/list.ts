61a7d3df336b58dfa5c6400661a31d0c04235782
Nicolas Mahe <nicolas@mahe.me>
2019-11-26 16:47:53 +0700

    Show runners in service list command of CLI

--- packages/cli/src/commands/service/list.ts
+++ packages/cli/src/commands/service/list.ts
@@ -20,18 +20,31 @@ export default class ServiceList extends Command {
       this.api.runner.list({}),
     ])
     cli.table(services || [], {
-      hash: {header: 'HASH', get: srv => srv.hash ? base58.encode(srv.hash) : ''},
+      hash: {header: 'HASH', get: srv => base58.encode(srv.hash)},
       sid: {header: 'SID', get: srv => srv.sid},
       instances: {
         header: 'INSTANCES',
         get: srv => (instances || [])
-          .filter(inst => inst.serviceHash && srv.hash && inst.serviceHash.toString() === srv.hash.toString())
-          .map(inst => base58.encode(inst.hash) + '. Runners: ' + (runners || [])
-            .filter(run => run.instanceHash && inst.hash && run.instanceHash.toString() === inst.hash.toString())
-            .map(run => run.hash && base58.encode(run.hash))
-            .join(" - "))
-          .join('\n')
+          .filter(inst => inst.serviceHash.toString() === srv.hash.toString())
+          .map(inst => [
+            base58.encode(inst.hash),
+            (runners || [])
+              .filter(run => run.instanceHash.toString() === inst.hash.toString())
+              .reduce((p, _, i) => p + (i > 0 ? '\n' : ''), '')
+          ].join(''))
+        .join('\n'),
       },
+      runners: {
+        header: 'RUNNERS',
+        get: srv => (instances || [])
+          .filter(inst => inst.serviceHash.toString() === srv.hash.toString())
+          .map(inst => (runners || [])
+            .filter(run => run.instanceHash.toString() === inst.hash.toString())
+            .map(run => base58.encode(run.hash))
+            .join('\n'),
+          )
+          .join('\n'),
+      }
     }, {printLine: this.log, ...flags})
     return instances || []
   }
