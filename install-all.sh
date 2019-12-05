73e0fdfb8fe83445cbefca0b20655d3cb9297c91
roms13 <romain.kelifa@gmail.com>
2019-12-05 19:41:16 +0700

    Use lerna bootstrap as requested in #146 comment

--- install-all.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-for d in ./packages/* ; do
-  if [[ -d "$d" && -f "$d/package.json" ]]; then
-    echo "$(tput setaf 2)installing $d$(tput sgr0)"
-    cd "$d"
-    npm install
-    cd ../..
-  fi
-done
