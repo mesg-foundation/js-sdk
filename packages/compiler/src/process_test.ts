2f11d37b61899b2de24f9f5b7af729ec044cf002
Anthony ESTEBE <anthony@mesg.com>
2019-12-13 14:02:05 +0700

    add process tests

--- packages/compiler/src/process_test.ts
+++ packages/compiler/src/process_test.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 import compile from './process'
 
 test('valid compilation', async (t: Test) => {
-  t.plan(58)
+  t.plan(94)
   const env = { INSTANCE_HASH: 'xxx' }
   const res = await compile(
     readFileSync('src/tests/process.yml'),
@@ -40,7 +40,7 @@ test('valid compilation', async (t: Test) => {
 
   // Test map
   t.equal(map.key, "node-2-inputs")
-  t.equal(Object.keys(map.map.outputs).length, 7)
+  t.equal(Object.keys(map.map.outputs).length, 12)
   // Test constants
   t.equal(map.map.outputs["constant_str"].stringConst, "constant_str")
   t.equal(map.map.outputs["constant_null"].null, 0)
@@ -54,7 +54,7 @@ test('valid compilation', async (t: Test) => {
   t.equal(list[2].doubleConst, 42)
   t.equal(list[3].map.outputs["foo"].stringConst, "bar")
   t.equal(list[3].map.outputs["number"].doubleConst, 42)
-  t.equal(list[4].ref.key, "dataX")
+  t.equal(list[4].ref.path.key, "dataX")
   t.equal(list[4].ref.nodeKey, "eventTrigger")
   // Test map
   const inputMap = map.map.outputs["constant_map"].map.outputs
@@ -68,7 +68,7 @@ test('valid compilation', async (t: Test) => {
   t.equal(inputMap["constant_list"].list.outputs[1].null, 0)
   t.equal(inputMap["constant_list"].list.outputs[2].doubleConst, 42)
   t.equal(inputMap["constant_list"].list.outputs[3].map.outputs["foo"].stringConst, "bar")
-  t.equal(inputMap["constant_list"].list.outputs[4].ref.key, "dataX")
+  t.equal(inputMap["constant_list"].list.outputs[4].ref.path.key, "dataX")
   t.equal(inputMap["constant_list"].list.outputs[4].ref.nodeKey, "eventTrigger")
   t.equal(inputMap["constant_map"].map.outputs["constant_str"].stringConst, "constant_str")
   t.equal(inputMap["constant_map"].map.outputs["constant_null"].null, 0)
@@ -78,12 +78,58 @@ test('valid compilation', async (t: Test) => {
   t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[1].null, 0)
   t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[2].doubleConst, 42)
   t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[3].map.outputs["foo"].stringConst, "bar")
-  t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[4].ref.key, "dataX")
+  t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[4].ref.path.key, "dataX")
   t.equal(inputMap["constant_map"].map.outputs["constant_list"].list.outputs[4].ref.nodeKey, "eventTrigger")
-  t.equal(inputMap["constant_map"].map.outputs["ref"].ref.key, "dataX")
+  t.equal(inputMap["constant_map"].map.outputs["ref"].ref.path.key, "dataX")
   t.equal(inputMap["constant_map"].map.outputs["ref"].ref.nodeKey, "eventTrigger")
   // Test ref
   const ref = map.map.outputs["ref"].ref
-  t.equal(ref.key, "dataX")
+  t.equal(ref.path.key, "dataX")
   t.equal(ref.nodeKey, "eventTrigger")
+  // Test ref nested
+  const ref_nested = map.map.outputs["ref_nested"].ref
+  t.equal(ref.nodeKey, "eventTrigger")
+  t.equal(ref_nested.path.key, "dataX")
+  t.equal(ref_nested.path.index, null)
+  t.equal(ref_nested.path.path.key, "foo")
+  t.equal(ref_nested.path.path.path, null)
+  t.equal(ref_nested.path.path.index, null)
+  // Test ref list
+  const ref_list = map.map.outputs["ref_list"].ref
+  t.equal(ref.nodeKey, "eventTrigger")
+  t.equal(ref_list.path.key, "dataY")
+  t.equal(ref_list.path.index, null)
+  t.equal(ref_list.path.path.key, null)
+  t.equal(ref_list.path.path.path, null)
+  t.equal(ref_list.path.path.index, 0)
+  // Test ref nested_list
+  const ref_nested_list = map.map.outputs["ref_nested_list"].ref
+  t.equal(ref.nodeKey, "eventTrigger")
+  t.equal(ref_nested_list.path.key, "dataX")
+  t.equal(ref_nested_list.path.index, null)
+  t.equal(ref_nested_list.path.path.key, "bar")
+  t.equal(ref_nested_list.path.path.index, null)
+  t.equal(ref_nested_list.path.path.path.key, null)
+  t.equal(ref_nested_list.path.path.path.path, null)
+  t.equal(ref_nested_list.path.path.path.index, 0)
+  // Test ref list nested
+  const ref_list_nested = map.map.outputs["ref_list_nested"].ref
+  t.equal(ref.nodeKey, "eventTrigger")
+  t.equal(ref_list_nested.path.key, "dataY")
+  t.equal(ref_list_nested.path.index, null)
+  t.equal(ref_list_nested.path.path.key, null)
+  t.equal(ref_list_nested.path.path.index, 0)
+  t.equal(ref_list_nested.path.path.path.key, "bar")
+  t.equal(ref_list_nested.path.path.path.path, null)
+  t.equal(ref_list_nested.path.path.path.index, null)
+  // Test ref list nested
+  const ref_list_list = map.map.outputs["ref_list_list"].ref
+  t.equal(ref.nodeKey, "eventTrigger")
+  t.equal(ref_list_list.path.key, "dataY")
+  t.equal(ref_list_list.path.index, null)
+  t.equal(ref_list_list.path.path.key, null)
+  t.equal(ref_list_list.path.path.index, 0)
+  t.equal(ref_list_list.path.path.path.key, null)
+  t.equal(ref_list_list.path.path.path.path, null)
+  t.equal(ref_list_list.path.path.path.index, 1)
 });
