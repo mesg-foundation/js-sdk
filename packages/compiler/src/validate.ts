658159b798796a77f90e3b2397f1cb5da88f359d
Nicolas Mahe <nicolas@mahe.me>
2020-01-28 12:52:02 +0700

    Return all schema validation error in compiler

--- packages/compiler/src/validate.ts
+++ packages/compiler/src/validate.ts
@@ -1,15 +1,10 @@
 import Avg from 'ajv'
 
 export default (schema: object, data: any) => {
-  const validate = new Avg().compile(schema)
+  const validate = new Avg({
+    jsonPointers: true
+  }).compile(schema)
   if (validate(data)) return
-  // Get the error with the longest schema path to avoid having a list of errors that might not be related
-  const error = validate.errors.reduce((prev, error) => prev
-    ? prev.schemaPath.split('/').length > error.schemaPath.split('/').length
-      ? prev
-      : error
-    : error
-  , null)
-  if (!error) return
-  throw new Error(`${error.dataPath} ${error.message}`)
+  const error = validate.errors.reduce((prev, error) => `${prev}\n${error.dataPath} ${error.message}`, "")
+  throw new Error(error)
 }
