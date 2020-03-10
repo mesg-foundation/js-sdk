75dd44956e4b1f68736a553afbbe6cacb30ceca9
antho1404 <anthony@mesg.com>
2020-03-10 12:24:53 +0700

    add helper to parse logs of transaction

---
+++ packages/cli/src/utils/txevent.ts
@@ -0,0 +1,13 @@
+import { TxResult } from "@mesg/api/lib/lcd";
+
+const isAction = (action: string) => (x: any) => x.key === 'action' && x.value === action
+const isModule = (module: string) => (x: any) => x.key === 'module' && x.value === module
+const isHash = (x: any) => x.key === 'hash'
+
+export const findHash = (txResult: TxResult, resourceType: 'service', action: string): string => {
+  const event = txResult.logs
+    .map(x => x.events.find(y => y.type === 'message').attributes)
+    .find(x => x.find(isAction(action)) && x.find(isModule(resourceType)) && x.find(isHash))
+  if (!event) throw new Error(`cannot find ${resourceType}\'s hash`)
+  return event.find(isHash).value
+}
