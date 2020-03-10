47a939a3f4575868acc1a3c4eb92889068a01d62
Anthony <anthony@mesg.com>
2020-03-10 14:33:54 +0700

    Update packages/api/src/lcd.ts

--- packages/api/src/lcd.ts
+++ packages/api/src/lcd.ts
@@ -15,7 +15,7 @@ class API {
   ownership: Ownership
   account: Account
 
-  constructor(endpoint: string) {
+  constructor(endpoint?: string) {
     this.service = new Service(endpoint)
     this.instance = new Instance(endpoint)
     this.runner = new Runner(endpoint)
