1e373ada0cc7cdeffbd09effaf477ba975d52727
antho1404 <anthony@mesg.com>
2020-03-19 14:18:19 +0700

    reorganize service execute command

--- packages/cli/src/commands/service/execute.ts
+++ packages/cli/src/commands/service/execute.ts
@@ -1,20 +1,18 @@
 import { flags, Command } from '@oclif/command'
 import { readFileSync } from 'fs'
-import { ExecutionCreateOutputs } from '@mesg/api/lib/execution'
 import { cli } from 'cli-ux'
 import LCD from '@mesg/api/lib/lcd'
 import Application from '@mesg/application'
 import API from '@mesg/api'
+import { decode } from '@mesg/api/lib/util/encoder'
 import Listr from 'listr'
-import * as Execution from '../../tasks/execution'
-import * as Runner from '../../tasks/runner'
-import * as Service from '../../tasks/service'
-import * as Instance from '../../tasks/instance'
-import { IService } from '@mesg/api/lib/service-lcd'
+import * as base58 from "@mesg/api/lib/util/base58";
 import ServiceType from "@mesg/api/lib/typedef/service";
-
-export type IValidateTask = { service: IService, taskKey: string, task?: ServiceType.mesg.types.Service.ITask }
-export type Context = Runner.IGet | Instance.IGet | Service.IGet | IValidateTask | Execution.IConvertInput | Execution.IExecute
+import ExecutionType from "@mesg/api/lib/typedef/execution";
+import { resolveSIDRunner } from "@mesg/api/lib/util/resolve";
+import { IRunner } from '@mesg/api/lib/runner-lcd'
+import { IExecution } from '@mesg/api/lib/execution'
+import { convert } from '../../utils/input'
 
 export default class Execute extends Command {
   static description = 'Execute a task on a running service'
@@ -42,46 +40,59 @@ export default class Execute extends Command {
     description: 'Task key'
   }]
 
-  async run(): ExecutionCreateOutputs {
+  private lcd = new LCD(`http://localhost:1317`)
+  private app = new Application(new API(`localhost:50052`))
+
+  async run() {
     const { args, flags } = this.parse(Execute)
-    const app = new Application(new API(`localhost:50052`))
 
-    const tasks = new Listr<Context>([
+    let runner: IRunner
+    let task: ServiceType.mesg.types.Service.ITask
+    let inputs: ExecutionType.mesg.protobuf.IStruct
+    let execution: IExecution
+
+    const tasks = new Listr([
       {
-        title: 'Validate task',
-        task: () => new Listr<Runner.IGet | Instance.IGet | Service.IGet | IValidateTask>([
-          Runner.get,
-          Instance.get,
-          Service.get,
-          {
-            title: 'Validate task',
-            task: (ctx: IValidateTask) => {
-              ctx.task = ctx.service.tasks.find(x => x.key === ctx.taskKey)
-              if (!ctx.task) throw new Error(`The task ${ctx.taskKey} does not exist in service '${ctx.service.hash}'`)
-            }
+        title: 'Validating task',
+        task: async () => {
+          try {
+            runner = await this.lcd.runner.get(args.RUNNER_HASH)
+          } catch (err) {
+            const hash = await resolveSIDRunner(this.lcd, args.RUNNER_HASH)
+            runner = await this.lcd.runner.get(hash)
           }
-        ])
+          const instance = await this.lcd.instance.get(runner.instanceHash)
+          const service = await this.lcd.service.get(instance.serviceHash)
+          task = service.tasks.find(x => x.key === args.TASK)
+          if (!task) this.error(`The task ${args.TASK} does not exist in service '${service.hash}'`)
+        }
       },
-      Execution.convertInput,
-      Execution.execute,
+      {
+        title: 'Converting inputs',
+        task: () => {
+          inputs = convert(task, flags.json
+            ? JSON.parse(readFileSync(flags.json).toString())
+            : (flags.data || []).reduce((acc, item) => ({
+              ...acc,
+              [item.split('=')[0]]: item.split('=')[1]
+            }), {}))
+        }
+      },
+      {
+        title: 'Executing task',
+        task: async () => {
+          execution = await this.app.executeTaskAndWaitResult({
+            eventHash: base58.decode(flags.eventHash || '6aUPZhmnFKiSsHXRaddbnqsKKi9KogbQNiKUcpivaohb'),
+            executorHash: base58.decode(runner.hash),
+            inputs,
+            tags: ['CLI'],
+            taskKey: args.TASK
+          })
+        }
+      }
     ])
-    const res = await tasks.run({
-      lcd: new LCD(`http://localhost:1317`),
-      runnerHash: args.RUNNER_HASH,
-      taskKey: args.TASK,
-      app: app,
-      data: flags.json
-        ? JSON.parse(readFileSync(flags.json).toString())
-        : (flags.data || []).reduce((acc, item) => ({
-          ...acc,
-          [item.split('=')[0]]: item.split('=')[1]
-        }), {}),
-      eventHash: flags.eventHash || '6aUPZhmnFKiSsHXRaddbnqsKKi9KogbQNiKUcpivaohb',
-      tags: ['CLI'],
-    })
+    await tasks.run()
 
-    const result = app.decodeData((res as Execution.IExecute).result.outputs)
-    cli.styledJSON(result)
-    return result
+    cli.styledJSON(decode(execution.outputs))
   }
 }
