b044d393872eddfb2645279bc0f9af191dac6bc2
antho1404 <anthony@mesg.com>
2020-04-08 12:03:55 +0700

    normalize payload for the orchestrator

--- packages/orchestrator/src/execution.ts
+++ packages/orchestrator/src/execution.ts
@@ -1,18 +1,63 @@
 import * as grpc from 'grpc'
+import * as base58 from '@mesg/api/lib/util/base58'
+import * as encoder from '@mesg/api/lib/util/encoder'
 import { Client } from './client'
 import * as Type from './typedef/execution'
 import * as API from './api/execution'
+
+// TODO: To improve using the types but if include the type, the definition is still included in the js
+export const Status: { [key: string]: Type.mesg.types.Status } = {
+  Unknown: 0, // Type.mesg.types.Status.Unknown,
+  Proposed: 1, // Type.mesg.types.Status.Proposed,
+  InProgress: 2, // Type.mesg.types.Status.InProgress,
+  Completed: 3, // Type.mesg.types.Status.Completed,
+  Failed: 4, // Type.mesg.types.Status.Failed
+}
+
+export type CreateRequest = {
+  executorHash: string;
+  taskKey: string;
+  inputs: Object;
+  tags?: string[];
+  price?: string;
+}
+
+export type StreamRequest = {
+  filter: {
+    statuses?: Type.mesg.types.Status[]
+    instanceHash?: string
+    taskKey?: string
+    tags?: string[]
+    executorHash?: string
+  }
+}
 
 export default class Execution extends Client {
   constructor(endpoint: string) {
     super(endpoint, 'Execution')
   }
 
-  public async create(request: API.mesg.grpc.orchestrator.IExecutionCreateRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IExecutionCreateResponse> {
-    return this.unaryCall("Create", request, signature)
+  public async create(request: CreateRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IExecutionCreateResponse> {
+    const gprcReq: API.mesg.grpc.orchestrator.IExecutionCreateRequest = {
+      executorHash: base58.decode(request.executorHash),
+      inputs: encoder.encode(request.inputs),
+      price: request.price,
+      tags: request.tags,
+      taskKey: request.taskKey
+    }
+    return this.unaryCall("Create", gprcReq, signature)
   }
 
-  public stream(request: API.mesg.grpc.orchestrator.IExecutionStreamRequest, signature: string): grpc.ClientReadableStream<Type.mesg.types.IExecution> {
-    return this.streamCall('Stream', request, signature)
+  public stream(request: StreamRequest, signature: string): grpc.ClientReadableStream<Type.mesg.types.IExecution> {
+    const grpcReq: API.mesg.grpc.orchestrator.IExecutionStreamRequest = {
+      filter: {
+        executorHash: request.filter.executorHash ? base58.decode(request.filter.executorHash) : null,
+        instanceHash: request.filter.instanceHash ? base58.decode(request.filter.instanceHash) : null,
+        statuses: request.filter.statuses,
+        tags: request.filter.tags,
+        taskKey: request.filter.taskKey
+      }
+    }
+    return this.streamCall('Stream', grpcReq, signature)
   }
 }
--- packages/orchestrator/src/runner.ts
+++ packages/orchestrator/src/runner.ts
@@ -1,16 +1,33 @@
 import { Client } from './client'
+import * as base58 from '@mesg/api/lib/util/base58'
 import * as API from './api/runner'
+
+export type RegisterRequest = {
+  serviceHash: string
+  envHash: string
+}
+
+export type DeleteRequest = {
+  runnerHash: string
+}
 
 export default class Runner extends Client {
   constructor(endpoint: string) {
     super(endpoint, 'Runner')
   }
 
-  public async register(request: API.mesg.grpc.orchestrator.IRunnerRegisterRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IRunnerRegisterResponse> {
-    return this.unaryCall("Register", request, signature)
+  public async register(request: RegisterRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IRunnerRegisterResponse> {
+    const grpcReq: API.mesg.grpc.orchestrator.IRunnerRegisterRequest = {
+      envHash: base58.decode(request.envHash),
+      serviceHash: base58.decode(request.serviceHash)
+    }
+    return this.unaryCall("Register", grpcReq, signature)
   }
 
-  public async delete(request: API.mesg.grpc.orchestrator.IRunnerDeleteRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IRunnerRegisterResponse> {
-    return this.unaryCall("Delete", request, signature)
+  public async delete(request: DeleteRequest, signature: string): Promise<API.mesg.grpc.orchestrator.IRunnerRegisterResponse> {
+    const grpcReq: API.mesg.grpc.orchestrator.IRunnerDeleteRequest = {
+      runnerHash: base58.decode(request.runnerHash)
+    }
+    return this.unaryCall("Delete", grpcReq, signature)
   }
 }
