246c660bf6d91e1a8ee69480cabc8386f5fa31a9
antho1404 <anthony@mesg.com>
2020-04-12 20:40:55 +0700

    deploy and start service on firebase

--- packages/cli/src/commands/deploy/service.ts
+++ packages/cli/src/commands/deploy/service.ts
@@ -5,7 +5,6 @@ import { IDefinition } from '@mesg/api/lib/service'
 import { compile } from '../../utils/service'
 import { loginFromCredential } from '../../utils/login'
 import firebase from '../../utils/firebase'
-import styledJSON from 'cli-ux/lib/styled/json'
 
 const ipfsClient = require('ipfs-http-client')
 
@@ -33,7 +32,7 @@ export default class Service extends Command {
 
 
     let definition: IDefinition
-    let res: any
+    let serviceHash: string
 
     const { user } = await loginFromCredential(this.config.configDir, password)
     const tasks = new Listr([
@@ -45,18 +44,65 @@ export default class Service extends Command {
       },
       {
         title: 'Creating service',
-        task: async (ctx, task) => {
-          res = await firebase.firestore().collection('services').add({
+        task: async () => {
+          const res = await firebase.firestore().collection('services').add({
             uid: user.uid,
             definition
           })
+          serviceHash = await new Promise((resolve, reject) => {
+            firebase.firestore()
+              .collection('services').doc(res.id)
+              .onSnapshot((snapshot) => {
+                const data = snapshot.data()
+                if (!data.hash) return
+                resolve(data.hash)
+              }, reject)
+          })
         }
-      },
+      }
     ])
     await tasks.run()
 
-    styledJSON(res)
-    // this.log(`Service deployed with the hash ${service.hash}`)
-    // this.log(`https://explorer.testnet.mesg.com/services/${service.hash}`)
+    this.log('')
+    this.log(`Service deployed with the hash ${serviceHash}`)
+    this.log('')
+
+    const { deploy } = await prompt({ type: 'confirm', name: 'deploy', message: 'Would you like to start this service?' })
+
+    if (!deploy) return
+
+    const env = await this.getEnv(definition.configuration.env || [])
+
+    const res = await firebase.firestore().collection('runners').add({
+      uid: user.uid,
+      serviceHash: serviceHash,
+      env: env
+    })
+
+    const runnerHash = await new Promise((resolve, reject) => {
+      firebase.firestore()
+        .collection('runners').doc(res.id)
+        .onSnapshot((snapshot) => {
+          const data = snapshot.data()
+          if (!data.hash) return
+          resolve(data.hash)
+        }, reject)
+    })
+
+    this.log('')
+    this.log(`Service started with the runner hash ${runnerHash}`)
+
+    await firebase.firestore().terminate()
+  }
+
+  async getEnv(envs: string[]): Promise<string[]> {
+    if (!envs.length) return []
+
+    const res = await prompt(envs.map((x: string) => ({
+      name: x.split('=')[0],
+      default: x.split('=')[1],
+      type: 'input'
+    }))) as { [key: string]: string }
+    return Object.keys(res).map(x => `${x}=${res[x]}`)
   }
 }
